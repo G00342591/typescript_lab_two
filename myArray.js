7c5ad381758bcda0f9bc76beea2e998638af7527
MATTHEW BARTLEY - STUDENT <G00342591@gmit.ie>
2018-01-31 17:08:01 +0000

    Commit 3

---
+++ myArray.js
@@ -0,0 +1,29 @@
+"use strict";
+var myArray = [];
+function addTask(task) {
+    myArray.push(task);
+    console.log("Item Added: " + task);
+    return myArray.length;
+}
+function listAllTasks() {
+    for (var _i = 0, myArray_1 = myArray; _i < myArray_1.length; _i++) {
+        var item = myArray_1[_i];
+        console.log(item);
+    }
+    console.log("\n");
+}
+function deleteTask(task) {
+    var index = myArray.indexOf(task);
+    if (index > -1) {
+        myArray.splice(index, 1);
+    }
+    return myArray.length;
+}
+addTask("Run");
+addTask("Walk");
+addTask("Hop");
+addTask("Jump");
+addTask("Crawl");
+listAllTasks();
+deleteTask("Hop");
+listAllTasks();
---
+++ myArray.ts
@@ -0,0 +1,37 @@
+let myArray:Array<String>= [];
+
+function addTask(task:string):number
+{
+    myArray.push(task);
+    console.log("Item Added: "+task)
+
+    return myArray.length;
+}
+
+function listAllTasks()
+{
+    for(let item of myArray)
+    {
+        console.log(item)
+    }
+    console.log("\n")
+}
+
+function deleteTask(task:String):number
+{
+    let index : number = myArray.indexOf(task);
+    if(index > -1)
+    {
+        myArray.splice(index,1);
+    }
+    return myArray.length;
+}
+
+addTask("Run");
+addTask("Walk");
+addTask("Hop");
+addTask("Jump");
+addTask("Crawl");
+listAllTasks();
+deleteTask("Hop");
+listAllTasks();
